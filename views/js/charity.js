04197b538a969121fe6318503dd8326a1f03fd9b
Nathan Bell <nat.bell@gmail.com>
2021-09-15 10:57:01 +1000

    finish charity page

---
+++ views/js/charity.js
@@ -0,0 +1,24 @@
+$.get('/charities', function (data, textStatus, jqXHR) {
+    // success callback
+    console.log(data);
+    insertCharities(data);
+});
+
+
+//insert charities data into cards
+const insertCharities = (charities) => {
+    charities.forEach((charity) => {
+        $('#charity-collection').append(
+            `<div class="card">
+            <span class="card-title" id="cardtitle">${charity.name}</span>
+                <div class="card-image">
+                    <img src="${charity.image}">
+                </div>
+                <div class="card-content">${charity.introduction}</div>
+                <div class="card-action">
+                    <a href="${charity.url}" target="_blank">${charity.name} <i class="material-icons tiny">open_in_new</i></a>
+                </div>
+            </div>`,
+        );
+    });
+};
