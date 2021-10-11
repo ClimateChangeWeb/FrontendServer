f517bf2c99f4b74a107c944c2cc01c87f7e58439
Ray Guo <ma2808203259@hotmail.com>
2021-10-11 23:44:55 +0800

    added audio for homepage

--- views/js/index.js
+++ views/js/index.js
@@ -7,3 +7,17 @@ $(document).ajaxStop(function () {
   // alert('load finished');
   console.log(`load finished`);
 });
+
+function playAudio(audioId) {
+  const audioList = ['audio1', 'audio2', 'audio3'];
+  const index = audioList.indexOf(audioId);
+
+  audioList.splice(index, 1);
+
+  audioList.forEach(function (id) {
+    document.getElementById(id).pause();
+    document.getElementById(id).currentTime = 0;
+  });
+  const audio = document.getElementById(audioId);
+  audio.paused ? audio.play() : audio.pause();
+}
